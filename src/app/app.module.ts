18a473d34af5684c5ca9ade85336dc8cfe19c3b0
hiepblack <chuongduong0123@gmail.com>
2023-05-27 12:14:46 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/interface/todo.ts
@@ -0,0 +1,6 @@
+export interface ITodos {
+    id?:number;
+    userId:string | number;
+    title:string,
+    completed:boolean
+}
